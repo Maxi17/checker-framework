8f866f81996ed24bfe2fdfbc653f63b720c6f916
Maximilian Gama <maxi.gama@yahoo.com>
2019-07-30 23:52:13 +0300

    Improved test case

--- checker/tests/nullness/Issue2587.java
+++ checker/tests/nullness/Issue2587.java
@@ -19,6 +19,16 @@ public abstract class Issue2587 {
     public static class Inner {
         // :: error: (expression.unparsable.type.invalid) :: error: (assignment.type.incompatible)
         @KeyFor("myMap") String MY_KEY = "";
+
+        public static class Inner2 {
+            // :: error: (expression.unparsable.type.invalid) :: error:
+            // (assignment.type.incompatible)
+            @KeyFor("myMap") String MY_KEY2 = "";
+            // :: error: (assignment.type.incompatible)
+            @KeyFor("innerMap") String MY_KEY3 = "";
+        }
+
+        private static final Map<String, Integer> innerMap = new HashMap<>();
     }
 
     private final Map<String, Integer> myMap = new HashMap<>();
