a3d41e7cc8d5602729209022be34ff4704dd5dd6
Maxi17 <maxi.gama@yahoo.com>
2019-12-10 00:59:39 +0000

    Added aliases

--- framework/src/main/java/org/checkerframework/common/purity/PurityAnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/common/purity/PurityAnnotatedTypeFactory.java
@@ -6,18 +6,49 @@ import java.util.HashSet;
 import java.util.Set;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.common.purity.qual.Deterministic;
+import org.checkerframework.common.purity.qual.Pure;
 import org.checkerframework.common.purity.qual.PurityUnqualified;
+import org.checkerframework.common.purity.qual.SideEffectFree;
+import org.checkerframework.common.purity.qual.TerminatesExecution;
+import org.checkerframework.javacutil.AnnotationBuilder;
 
 /** Partial documentation to make tests pass */
 public class PurityAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
     public PurityAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
+
+        addAliasedAnnotation(
+                Pure.class,
+                AnnotationBuilder.fromClass(
+                        getElementUtils(), org.checkerframework.dataflow.qual.Pure.class));
+        addAliasedAnnotation(
+                Deterministic.class,
+                AnnotationBuilder.fromClass(
+                        getElementUtils(), org.checkerframework.dataflow.qual.Deterministic.class));
+        addAliasedAnnotation(
+                SideEffectFree.class,
+                AnnotationBuilder.fromClass(
+                        getElementUtils(),
+                        org.checkerframework.dataflow.qual.SideEffectFree.class));
+        addAliasedAnnotation(
+                TerminatesExecution.class,
+                AnnotationBuilder.fromClass(
+                        getElementUtils(),
+                        org.checkerframework.dataflow.qual.TerminatesExecution.class));
+
         this.postInit();
     }
 
     @Override
     protected Set<Class<? extends Annotation>> createSupportedTypeQualifiers() {
-        return new HashSet<>(Arrays.asList(PurityUnqualified.class));
+        return new HashSet<>(
+                Arrays.asList(
+                        PurityUnqualified.class,
+                        Pure.class,
+                        Deterministic.class,
+                        SideEffectFree.class,
+                        TerminatesExecution.class));
     }
 }
